20e5b998c976fceb90af0c2747ebd1ba50a3bf61
ahmed3520 <ataha352000@gmail.com>
2022-05-24 22:36:01 +0200

    update  landing page to  css module

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,44 +1,45 @@
 import React from 'react'
 import Nav from '../components/navbar/Nav'
+import landing from './landing.module.css'
 const Landing = () => {
   return (
     <>
     <Nav/>
-    <div className='dflex cont faround' style={{marginTop:'65px'}}>
-        <div className='left dflex fcol fcenter'>
-            <div className='con'>
-              <div className='all-h con'>
-            <h3 className='landing-h3'>
+    <div className={landing.cont} style={{marginTop:'65px'}}>
+        <div className={landing.left}>
+            <div className={landing.con}>
+              <div className={landing.con}>
+            <h3 className={landing.landing_h3}>
             Make Impact by teaching
             </h3>
-            <h5 className='h5'>
+            <h5 className={landing.landing_h5}>
             easy booking,easy payment for your scheduled live classes.lets handle all the stress for you while you do your work easily
             </h5>
             </div>
             <div className='land-btn'>
-            <button className='btn dbrwb'>
+            <button className={landing.landing_btn}>
                 Get started
             </button>
             </div>
             </div>
         </div>
-        <div className='right'>
-            <img className='lan-img' src='/sm-women.png'/>
-            <div className='fback dflex'>
-              <div className='s-line'>
+        <div className={landing.right}>
+            <img className={landing.landing_img} src='/sm-women.png'/>
+            <div className={landing.fback}>
+              <div className={landing.s_line}>
                 
               </div>
 
-              <div className='f-cont '>
-              <div className='v-line'>
+              <div className={landing.f_cont}>
+              <div className={landing.v_line}>
               </div>
-              <div className='desc'>
+              <div className={landing.desc}>
                 
-                <span>
+                <span className={landing.desc_span}>
                 "Thank you so much . Booking Classes has nevr been this easy for me.  is exactly what I have been looking for."
                 </span>
-                <div className='name'>
-                  <h5>
+                <div className={landing.name}>
+                  <h5 className={landing.name_h5}>
                   Gloria Rose
                   </h5>
                 </div>
@@ -47,8 +48,8 @@ const Landing = () => {
 
           
             </div>
-            <div className='arr-circ'>
-                <img className='red' src='/Vector 5.svg'/>
+            <div className={landing.arr_circ}>
+                <img className={landing.red} src='/Vector 5.svg'/>
                 </div>
         </div>
     </div>
